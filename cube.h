d6e344b7e94c8d1cf31bf41ca5b51c7dab7ffb7c
phbalanced <prachihonhaga@gmail.com>
2026-05-20 23:45:26 +0530

    moves func declared

--- cube.h
+++ cube.h
@@ -34,4 +34,42 @@ public:
     static string getMove(Move ind);
     void print() const;
     vector<Move> randomShuffle(unsigned int times);
+    RubiksCube &applyMove(Move move);
+    RubiksCube &invert(Move move);
+    virtual RubiksCube &f()=0;
+
+    virtual RubiksCube &fPrime()=0;
+
+    virtual RubiksCube &f2()=0;
+
+    virtual RubiksCube &b()=0;
+
+    virtual RubiksCube &bPrime()=0;
+
+    virtual RubiksCube &b2()=0;
+
+    virtual RubiksCube &l()=0;
+
+    virtual RubiksCube &lPrime()=0;
+
+    virtual RubiksCube &l2()=0;
+
+    virtual RubiksCube &r()=0;
+
+    virtual RubiksCube &rPrime()=0;
+
+    virtual RubiksCube &r2()=0;
+
+    virtual RubiksCube &u()=0;
+
+    virtual RubiksCube &uPrime()=0;
+
+    virtual RubiksCube &u2()=0;
+
+    virtual RubiksCube &d()=0;
+
+    virtual RubiksCube &dPrime()=0;
+
+    virtual RubiksCube &d2()=0;
+
 };
