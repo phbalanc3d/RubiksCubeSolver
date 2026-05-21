a3da646f477994583eeecf7274ac87d216875d34
phbalanced <prachihonhaga@gmail.com>
2026-05-21 10:47:10 +0530

    getmove done

--- cube.cpp
+++ cube.cpp
@@ -14,4 +14,66 @@ char RubiksCube::getColorChar(Color color){
         case Color::GREEN:
             return 'G';
     }
+    return ' ';
+}
+
+string RubiksCube::getMove(Move ind){
+
+    switch(ind){
+
+        case Move::L:
+            return "L";
+
+        case Move::LPrime:
+            return "L'";
+
+        case Move::L2:
+            return "L2";
+
+        case Move::R:
+            return "R";
+
+        case Move::RPrime:
+            return "R'";
+
+        case Move::R2:
+            return "R2";
+
+        case Move::U:
+            return "U";
+
+        case Move::UPrime:
+            return "U'";
+
+        case Move::U2:
+            return "U2";
+
+        case Move::D:
+            return "D";
+
+        case Move::DPrime:
+            return "D'";
+
+        case Move::D2:
+            return "D2";
+
+        case Move::F:
+            return "F";
+
+        case Move::FPrime:
+            return "F'";
+
+        case Move::F2:
+            return "F2";
+
+        case Move::B:
+            return "B";
+
+        case Move::BPrime:
+            return "B'";
+
+        case Move::B2:
+            return "B2";
+    }
+    return "";
 }
