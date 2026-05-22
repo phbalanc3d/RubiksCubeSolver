1872132c933bf8afb14689d11aaf8edfa285c9b4
Prachi Honhaga <honhagaprachi2005.24@kgpian.iitkgp.ac.in>
2026-05-22 23:37:30 +0530

    iddfs done

--- solvers/iddfs.h
+++ solvers/iddfs.h
@@ -1,3 +1,32 @@
+#pragma once
 #include<bits/stdc++.h>
 #include "../models/cube.h"
 #include "DFSSolver.h"
+
+template<typename T>
+class IDDFSSolver {
+
+    private:
+    int maxidepth;
+    vector<RubiksCube::MOVE> moves;
+
+    public:
+    T rubiksCube;
+
+    IDDFSSolver(T _rubiksCube, int _max_search_depth = 7) {
+        rubiksCube = _rubiksCube;
+        maxidepth = _max_search_depth;
+    }
+
+    vector<RubiksCube::MOVE> solve() {
+        for (int i = 1; i <= maxidepth; i++) {
+            DFSSolver<T> dfsSolver(rubiksCube, i);
+            moves = dfsSolver.solve();
+            if (dfsSolver.rubiksCube.isSolved()) {
+                rubiksCube = dfsSolver.rubiksCube;
+                break;
+            }
+        }
+        return moves;
+    }
+};
