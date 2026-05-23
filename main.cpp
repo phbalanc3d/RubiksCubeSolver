8fde17a97dd1de289c1e0215ad1bd2aec29afd7d
Prachi Honhaga <honhagaprachi2005.24@kgpian.iitkgp.ac.in>
2026-05-23 16:17:57 +0530

    lots changed

--- main.cpp
+++ main.cpp
@@ -1,23 +1,30 @@
 #include "models/cube1d.cpp"
-#include "solvers/dfssolver.h"
+#include "solvers/iddfs.h"
 
+#include <ctime>
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main(){
+    srand(time(0));
     RubiksCube1dArray cube1d;
-    cube1d.randomShuffle(3);
+    cube1d.randomShuffle(9);
     cube1d.print();
 
-    DFSSolver<RubiksCube1dArray> solver(cube1d,8);
+    IDDFSSolver<RubiksCube1dArray> solver(cube1d,8);
     vector<RubiksCube::Move> moves = solver.solve();
     cout<<"Solution:\n";
 
     for(auto move : moves){
-
         cout<<cube1d.getMove(move)<<" ";
     }
-
     cout<<"\n";
+    for(auto move: moves){
+        cube1d.applyMove(move);
+    }
+
+    cout<<cube1d.isSolved()<<endl;
+    cube1d.print();
     return 0;
 }
