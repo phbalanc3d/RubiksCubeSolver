58a3998666e41cac1c4f7af9c8a7eee1d565f1b9
Prachi Honhaga <honhagaprachi2005.24@kgpian.iitkgp.ac.in>
2026-05-24 11:11:24 +0530

    nibblearray.h file done

---
+++ patternDatabase/nibbleArray.h
@@ -0,0 +1,28 @@
+#pragma once
+#include<bits/stdc++.h>
+using namespace std;
+class NibbleArray{
+    private:
+    // the no of ele in the array
+    size_t size;
+    vector<uint8_t> arr;
+
+    public:
+    // all intialised to 15 by defalut
+    // all the 8bits are 1 by default
+    NibbleArray(const size_t size,const uint8_t val= 0xFF);
+
+    uint8_t get(const size_t pos) const;
+
+    void set(const size_t pos, const uint8_t val);
+    //retur pointer to the data
+    unsigned char *data();
+    //const version too
+    const unsigned char* data() const;
+    // Actual storage size in bytes
+    size_t storageSize() const;
+    //compressed nibble array to dest vector in their original form
+    void inflate(vector<uint8_t>& dest) const;
+    // reset nibble array to specific val , by default all 1s
+    void reset(const uint8_t val = 0xFF);
+};
