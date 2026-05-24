5fad82c3d94999e4488c4342c9a2929318c63e73
Prachi Honhaga <honhagaprachi2005.24@kgpian.iitkgp.ac.in>
2026-05-25 02:13:24 +0530

    corner pattern database implementation done

--- patternDatabase/CornerPatternDatabase.h
+++ patternDatabase/CornerPatternDatabase.h
@@ -2,3 +2,15 @@
 #include "../models/cube.h"
 #include "patternDatabase.h"
 #include "PermutationIndexer.h"
+using namespace std;
+
+class CornerPatternDatabase : public PatternDatabase {
+    //permutation indexer for 8 corners
+    PermutationIndexer<8> permIndexer;
+    public:
+        CornerPatternDatabase();
+
+        CornerPatternDatabase(uint8_t init_val);
+
+        uint32_t getDatabaseIndex(const RubiksCube& cube) const;
+};
