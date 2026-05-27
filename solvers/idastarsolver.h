6793d5dbc6bb4873941dba9f22de6c4763768601
Prachi Honhaga <honhagaprachi2005.24@kgpian.iitkgp.ac.in>
2026-05-28 00:23:22 +0530

    added guard for no soln in idastar

--- solvers/idastarsolver.h
+++ solvers/idastarsolver.h
@@ -1,20 +1,12 @@
 #pragma once
 
-#include <iostream>
-   #include <vector>
-   #include <string>
-   #include <algorithm>
-   #include <map>
-   #include <unordered_map>
-   #include <queue>
-   #include <stack>
-   #include <cmath>
-   #include <cassert>
-   #include <climits>
-   #include <functional>
-   #include <numeric>
+#include <vector>
+#include <string>
+#include <climits>
+
 #include "../models/cube.h"
 #include "../patternDatabase/CornerDB.h"
+
 using namespace std;
 
 template<typename T>
@@ -26,17 +18,19 @@ private:
     int bound;
     // Next bound
     int next_bound;
-    CornerPatternDatabase cornerDB;
+//chnage 
+    CornerDB<T> cornerDB;
 
 public:
 
     T rubiksCube;
+
     IDAstarSolver(T cube,string fileName){
         rubiksCube = cube;
-        cornerDB = CornerPatternDatabase(fileName);
+        cornerDB = CornerDB<T>(fileName);
     }
-    // dummy heuristic 
-    int heuristic(T cube){
+    //heuristic 
+    int heuristic(T& cube){
         return cornerDB.getNumMoves(cornerDB.getDatabaseIndex(cube));
     }
     //dfs
@@ -85,6 +79,8 @@ public:
             // Start DFS
             if(dfs(0))
                 return moves;
+            //if unsolvable then its the guard
+            if (next_bound == INT_MAX) return {};
             // Increase bound
             bound = next_bound;
         }
