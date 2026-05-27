bd94d8cfd5c33ede89b41ce5891d859984b38398
Prachi Honhaga <honhagaprachi2005.24@kgpian.iitkgp.ac.in>
2026-05-28 02:06:10 +0530

    updated CornerDB as it never loaded form disk and solved mistakes

--- patternDatabase/CornerDB.h
+++ patternDatabase/CornerDB.h
@@ -27,63 +27,77 @@ public:
 
     bool bfsAndStore(uint8_t maxDepth);
 
+    uint32_t getDatabaseIndex(const T& cube) const {
+        return cornerDB.getDatabaseIndex(cube);
+    }
+    uint8_t getNumMoves(uint32_t index) const {
+        return cornerDB.getNumMoves(index);
+    }
+
 };
 
 //template implementations inside the .h file because of linker error due to template class and its implementation in different files
 template<typename T>
 CornerDB<T>::CornerDB(string _fileName)
-        :
-        fileName(_fileName){}
+    : fileName(_fileName), cornerDB(0xFF) {
+         // FIX: load existing DB from disk
+    ifstream file(_fileName, ios::binary);
+
+    if (file.good()) {
+        file.close();
+        cornerDB.fromFile(_fileName);
+        cout << "[CornerDB] Loaded from " << _fileName << "\n";
+    }
+}
 
 
 template<typename T>
-CornerDB<T>::CornerDB(
-        string _fileName,
-        uint8_t init_val)
-        :
-        fileName(_fileName),
-        cornerDB(init_val){}
+CornerDB<T>::CornerDB(string _fileName,uint8_t init_val)
+        :fileName(_fileName),cornerDB(init_val){}
 
 
 template<typename T>
-bool CornerDB<T>::bfsAndStore(
-        uint8_t maxDepth) {
+bool CornerDB<T>::bfsAndStore(uint8_t maxDepth) {
     // default constructor for solved cube
     T cube;
     queue<T> q;
     q.push(cube);
-    cout << "BFS started" << endl;
+    cout << "[CornerDB]BFS started" << endl;
     // solved state index
     uint32_t index = cornerDB.getDatabaseIndex(cube);
     // depth of solved state intialy is 0
     cornerDB.setNumMoves(index, 0);
+
 while(!q.empty()) {
         T curr = q.front();
         q.pop();
         uint8_t currDepth = cornerDB.getNumMoves(cornerDB.getDatabaseIndex(curr));
         // stop deeper expansion cuz its childern wil have depth more than maxDepth
-        if(currDepth == maxDepth)
-            continue;
+        if(currDepth == maxDepth) continue;
+
 for(int move = 0;move < 18;move++) {
             T child = curr;
 // this creates the child like all the neighbours of the current cube after applying one move
             child.applyMove(RubiksCube::Move(move));
             uint32_t childIndex =cornerDB.getDatabaseIndex(child);
 //if unvisited then
+
  if(cornerDB.getNumMoves(childIndex)== 255) {
                 cornerDB.setNumMoves(childIndex,currDepth + 1);
                 q.push(child);
             }
         }
     }
+
     for(uint32_t i = 0;i < cornerDB.getSize();i++) {
         if(cornerDB.getNumMoves(i)== 255) {
             cornerDB.setNumMoves(i,maxDepth + 1);
         }
     }
-    cout << "Saving DB..." << endl;
+
+    cout << "[CornerDB]Saving DB..." << endl;
 cornerDB.toFile(fileName);
-cout << "DB saved!" << endl;
+cout << "[CornerDB]DB saved!" << endl;
 return true;
 }
 
